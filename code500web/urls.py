3282adc1a68c3276b05d1a78f802d27a9636f18c
Code500 <codefivehundred@gmail.com>
2018-07-12 02:06:13 +0800

    revert back namespace

--- code500web/urls.py
+++ code500web/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', include('blog.urls', namespace='blog')),
     url('', include('blog.urls')),
 ]
