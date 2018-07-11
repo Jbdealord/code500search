9cd0207116f257120397b4620d8a764d93031603
Code500 <codefivehundred@gmail.com>
2018-07-12 02:09:56 +0800

    fix for django 2

--- code500web/urls.py
+++ code500web/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('blog.urls', namespace='blog')),
     url('', include('blog.urls')),
 ]
