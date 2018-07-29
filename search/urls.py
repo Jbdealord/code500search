01e8323acedec1c80eb2bd5eb129677f04c6e199
Code500 <codefivehundred@gmail.com>
2018-07-29 08:54:13 +0800

    define get, update and delete method

--- search/urls.py
+++ search/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from .views import CreateView
+from .views import CreateView, DetailsView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
 	url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-
+	url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
